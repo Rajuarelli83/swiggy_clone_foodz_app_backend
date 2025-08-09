5738cf597075e9e0e7713aa805f3b728e491f228
Raju Arelli <raju_arelli@Rajus-MacBook-Air.local>
2025-08-09 18:08:47 +0530

    feat: Add CORS middleware to allow frontend requests

--- index.js
+++ index.js
@@ -9,22 +9,23 @@ const path= require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 
-const allowedOrigins = [
-  "http://localhost:1234",
-  process.env.FRONTEND_URL 
-];
+// const allowedOrigins = [
+//   "http://localhost:1234",
+//   process.env.FRONTEND_URL 
+// ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     }
+//     callback(new Error('Not allowed by CORS'));
+//   },
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"]
+// }));
 
 
 app.use(express.json());
